6bf2753a9b56e291ec8ee672199e0207591a9cb5
iamawumpas <130360693+iamawumpas@users.noreply.github.com>
2025-09-19 08:41:58 +1200

    more error correction

--- custom_components/metlink_explorer/__init__.py
+++ custom_components/metlink_explorer/__init__.py
@@ -4,7 +4,7 @@ from .const import DOMAIN
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})
-    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
